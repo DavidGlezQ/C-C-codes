ea8e09452f42a459cf7a97d121eaef6b97cb5400
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2020-06-27 15:28:37 -0500

    Actividad2PDF_Funciones
    Programa que lee una columna y un caracter y lo imprime en forma de piramide.

---
+++ Actividad2PDF_Funciones.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+int main()
+{
+	int columna = 1,i,a;
+	char caracter[10];
+	printf("David Alejandro Gonzalez Quezada\n");
+	printf("20110407\n");
+	printf("Programa que imprime las columnas ingresadas por el ususario.\n");
+	printf("R2 Actividad 2\n");
+	printf("Ingresa el numero de columnas que quieres mostrar en pantalla.\n");
+	scanf("%i",&columna);
+	printf("Ingresa el caracter que quieres imprimir.\n");
+	fflush(stdin);
+	gets(caracter);
+	system("cls");
+	printf("Tu numero ingresado es: %i\n", columna);
+	for (i = 1; i <= columna; i++){
+		for (a = 1; i >= a; a++) {
+			printf("%s",caracter);
+		}
+		printf("\n");
+	}
+	system("pause");
+}
