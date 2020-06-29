5958d497c6c3750dcbfad2f23b1ca401be5ab1c9
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2020-06-28 23:23:10 -0500

    28/06/2020

--- Actividad3PDF_Funciones.cpp
+++ Actividad3PDF_Funciones.cpp
@@ -1,5 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+int numero = 0, potencia = 0, res = 1;
+int calcularPotencia(int numero, int potencia);
 int main(){
 	printf("David Alejandro Gonzalez Quezada\n");
+    printf("20110407\n");
+    printf("Actividad PDF3 Funciones\n");
+    printf("Programa que calcula un numero elevado\n");
+    printf("Ingresa el primer valor que se elevara por el segundo\n");
+    scanf("%i",&numero);
+    printf("Ingresa la potencia\n");
+    scanf("%i",&potencia);
+    calcularPotencia(numero, potencia);
+}
+
+int calcularPotencia(int numero, int potencia){
+	for(int i = 1; i<=potencia; i++){
+    	res = res*numero;
+	}
+	printf("El resultado es: %i", res); 
+	return res;
 }
